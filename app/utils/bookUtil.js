79629ba9cf987c4d3d75443e3acea08b44aab2e9
pytong <pytong07@gmail.com>
2015-12-04 21:09:27 +0000

    Show books with images preferrably

--- app/utils/bookUtil.js
+++ app/utils/bookUtil.js
@@ -7,7 +7,7 @@ let	configAuth = require('../config/auth'),
 		key: configAuth.googleAuth.apiKey, // Your Google API key (Optional)
 	    field: "title", // Search in a specified field (title, author, publisher, subject or isbn) (Optional)
 	    offset: 0, // The position in the collection at which to start the list of results (Default: 0)
-	    limit: 1, // The maximum number of results to return (Max 40) (Defult: 10)
+	    limit: 10, // The maximum number of results to return (Max 40) (Defult: 10)
 	    type: "books", // Restrict results to books or magazines (Default: all)
 	    order:"relevance", // Order results by relevance or newest (Default: relevance)
 	    lang: "en" // Restrict results to a specified language (two-letter ISO-639-1 code) (Default: en)
@@ -22,7 +22,15 @@ module.exports = {
 			if(err) { return callback(false, errorMessage); }
 			if(result.length < 1) { return callback(false, "No book found."); }
 
-            let bookFound = result[0],
+            let bookFound,
+                book,
+                i = 0;
+
+            while(i < (result.length - 1) && typeof(result[i].thumbnail) === "undefined" ) {
+                i++;
+            }
+
+            bookFound = result[i];
             book = new Book();
             book.title = bookFound.title;
             book.authors = bookFound.authors;
@@ -35,7 +43,7 @@ module.exports = {
             });
 		});
     },
-    
+
     deleteBook: function(params, callback) {
         Book.remove(params, function(err, res) {
             if(err) { return callback(false); }
